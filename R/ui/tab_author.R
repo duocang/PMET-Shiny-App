f8f3f3bd6e2695649008851bfd374fbbc2c0bccc
Bai juyi <wangxuesong29@gmail.com>
2023-08-01 17:03:25 +0200

    update contributors

--- R/ui/tab_author.R
+++ R/ui/tab_author.R
@@ -25,7 +25,7 @@ tabPanel(
     p("e-mail: s.ott@warwick.ac.uk", style = "font-size:20px"),
     p(a("Prof. Dr. Patrick Schäfer", href = "https://www.uni-giessen.de/de/fbz/fb09/institute/phyto/mitarbeiter/leitung/schaefer-p", target = "_blank"), style = "font-size:25px"),
     p("email: Patrick.Schaefer@agrar.uni-giessen.de", style = "font-size:20px"),
-    p(a("WOOLLEY-ALLEN, KATE (PGR)", href = "https://warwick.ac.uk/fac/sci/sbdtc/people/students/2012/kate_allen/", target = "_blank"), style = "font-size:25px"),
+    p(a("Kate Woolley-Allen (PGR)", href = "https://warwick.ac.uk/fac/sci/sbdtc/people/students/2012/kate_allen/", target = "_blank"), style = "font-size:25px"),
     p("e-mail: k.woolley-allen@warwick.ac.uk", style = "font-size:20px"),
     p(a("Paul Brown", href = "https://warwick.ac.uk/fac/cross_fac/zeeman_institute/staffv2/paulbrown/", target = "_blank"), style = "font-size:25px"),
     p("e-mail: p.e.brown@warwick.ac.uk", style = "font-size:20px"),
