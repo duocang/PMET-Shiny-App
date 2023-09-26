093aeb50033c93ee0afcdb75f4dc9d0f05ac5dbe
Bai juyi <wangxuesong29@gmail.com>
2023-09-26 14:52:28 +0200

    Auto adapt the size of saved image text.

--- R/utils/motif_pair_plot_hetero.R
+++ R/utils/motif_pair_plot_hetero.R
@@ -6,6 +6,9 @@ MotifPairPlotHetero <- function(plot.data = NULL,
   colors <- c("#ed3333", "#11659a", "#1a6840", "#f9a633", "#8b2671", "#2f2f35")[seq_along(clusters)]
   names(colors) <- clusters
 
+  inch_pre_motif <- 20 / length(motifs) * 1.5
+  font_size <- inch_pre_motif * 22
+
   p <- plot.data %>%
     ggplot(aes(motif1, motif2, alpha = p_adj, fill = factor(cluster))) +
     geom_tile(color = "white") +
@@ -20,7 +23,9 @@ MotifPairPlotHetero <- function(plot.data = NULL,
       axis.line = element_blank(),
       axis.title.x = element_blank(),
       axis.title.y = element_blank(),
-      axis.text.x = element_text(angle = 90)
+      axis.text.x = element_text(angle = 90, size = font_size),  # 设置x轴文本字号
+      axis.text.y = element_text(size = font_size),              # 设置y轴文本字号
+      legend.text = element_text(size = 25)
     ) +
     # guides(fill = "none", alpha = "none") +
     coord_fixed() +
